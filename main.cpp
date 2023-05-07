3db7704bf170515ae758287d8fe6d7a5d5890ad3
董文杰 <@SUNSHINE>
2023-05-07 20:41:58 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+	int x;
+public:
+	A(int x = 3) :x(x)
+	{
+		cout << x << endl;
+	}
+	virtual ~A()
+	{
+		cout << -x << endl;
+	}
+};
+int  main()
+{
+	A a1, a2(4);
+}
