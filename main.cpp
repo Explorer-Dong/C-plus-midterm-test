704c699986da9a3c9b387382a4f6cf9350b1f0a0
董文杰 <@SUNSHINE>
2023-05-07 21:31:37 +0800

    更新4

--- main.cpp
+++ main.cpp
@@ -6,31 +6,30 @@ class A1
 	int x1;
 public:
 	A1(int x1) :x1(x1) { cout << x1 << endl; }
+	virtual ~A1() { cout << -x1 << endl; }
 };
-class A2
-{
-	int x2;
-public:
-	A2(int x2) :x2(x2) { cout << x2 << endl; }
-};
-
 class A
 {
-	A1 a1;
-	A2 a2;
+	A1* p1;
 	int x;
 public:
-	A(int x1, int x2) :a1(x1), a2(x2), x(x1 + x2)
+	A(int x1, int x) :x(x)
 	{
-		cout << x << endl;
+		p1 = new A1(x1);
+	}
+	virtual ~A()
+	{
+		delete p1;      cout << -x << endl;
 	}
 };
 int main()
 {
 	A a(1, 2);
 	A* p = new A(4, 5);
+	delete p;
 
 	return 0;
 }
+
 
 
