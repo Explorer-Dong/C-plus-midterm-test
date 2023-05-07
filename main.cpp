59ae0549e1f8170ac5e96f84971e547b8b33283e
董文杰 <@SUNSHINE>
2023-05-07 21:19:10 +0800

    更新3

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class A1
+{
+	int x1;
+public:
+	A1(int x1) :x1(x1) { cout << x1 << endl; }
+};
+class A2
+{
+	int x2;
+public:
+	A2(int x2) :x2(x2) { cout << x2 << endl; }
+};
+
 class A
 {
+	A1 a1;
+	A2 a2;
 	int x;
 public:
-	A(const int x) :x(x)
+	A(int x1, int x2) :a1(x1), a2(x2), x(x1 + x2)
 	{
 		cout << x << endl;
-	}
-	A(const A& obj) :x(obj.x + 1)
-	{
-		cout << x << endl;
-	}
-	virtual ~A()
-	{
-		cout << -x << endl;
 	}
 };
 int main()
 {
-	A* p1 = new A(3);
-	A* p2 = new A(*p1);
-	delete p1;
-	delete p2;
+	A a(1, 2);
+	A* p = new A(4, 5);
+
 	return 0;
 }
+
 
