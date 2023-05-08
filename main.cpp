33e5d3351a1aef060898ebf5d634a15b761e1ac0
董文杰 <@SUNSHINE>
2023-05-08 17:02:27 +0800

    更新9

--- main.cpp
+++ main.cpp
@@ -5,18 +5,21 @@ class A
 {
 	int x;
 public:
-	A(int x = 0) :x(x) { cout << x << endl; }
+	A(const int x) :x(x) { cout << x << endl; }
 	A(const A& obj) :x(obj.x + 1) { cout << x << endl; }
 	virtual ~A() { cout << -x << endl; }
 };
-A f(A a1)
+A& f(A a1)
 {
-	return a1;
+	A* p = new A(a1);
+	return *p;
 }
 int main()
 {
-	A a1;
-	A a2 = f(a1);
+	A a1(1);
+	A& a2 = f(a1);
+	delete& a2;
 
 	return 0;
 }
+
