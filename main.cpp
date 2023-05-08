f8c950c646095f26904018f6e702f62cd425bca5
董文杰 <@SUNSHINE>
2023-05-08 19:13:02 +0800

    更新10

--- main.cpp
+++ main.cpp
@@ -3,23 +3,32 @@ using namespace std;
 
 class A
 {
-	int x;
+	int* p;
 public:
-	A(const int x) :x(x) { cout << x << endl; }
-	A(const A& obj) :x(obj.x + 1) { cout << x << endl; }
-	virtual ~A() { cout << -x << endl; }
+	A(const int x = 0)
+	{
+		p = new int;  *p = x;
+	}
+	A(const A& obj)
+	{
+		p = new int;  *p = (*obj.p) + 1;
+	}
+	A& operator=(A& obj)
+	{
+		if (this == &obj) return *this;
+		*p = *obj.p + 10;
+		return *this;
+	}
+	virtual ~A()
+	{
+		cout << -(*p) << endl; delete p;
+	}
 };
-A& f(A a1)
-{
-	A* p = new A(a1);
-	return *p;
-}
 int main()
 {
 	A a1(1);
-	A& a2 = f(a1);
-	delete& a2;
+	A a2(a1), a3;
+	a3 = a2 = a1;
 
 	return 0;
 }
-
